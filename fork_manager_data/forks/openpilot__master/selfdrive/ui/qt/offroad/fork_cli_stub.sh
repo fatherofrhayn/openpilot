3f0cfe667681b53351ffd775ab258f88620408ec
fatherofrhayn <100546262+fatherofrhayn@users.noreply.github.com>
2025-04-27 14:51:58 -0400

    chore: clean all fork_manager forks for production baseline (empty except .gitkeep)

--- fork_manager_data/forks/openpilot__master/selfdrive/ui/qt/offroad/fork_cli_stub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Stub for testing fork listing UI
-# Usage: export OPENPILOT_FORK_CLI=<path_to_this_script>
-
-echo "openpilot [manager] at /Users/jblair/Github/openpilot"
-echo "openpilot [master] at /Users/jblair/Github/openpilot/fork_manager_data/forks/openpilot__master"
